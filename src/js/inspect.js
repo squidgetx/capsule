5acbb0dcdb27c1241c66b483eac7fb77c8fa0ce3
Sylvan Zheng <squidgetx@gmail.com>
2023-03-25 20:23:09 -0400

    inspect kind of works

--- src/js/inspect.js
+++ src/js/inspect.js
@@ -6,14 +6,13 @@ import { pickRandom } from "./util"
 
 export const inspect = (Player, Map) => {
     const currentTile = Player.currentTile
-    const neighbors = Map.getAdjacentTiles(currentTile)
-    console.log(neighbors)
     const signals = Map.getSignals(currentTile)
 
     let str = [];
 
-    if (currentTile.star) {
-        str.push("There's a healthy star system here. This is a good spot to replenish our solar energy systems.")
+    let i = currentTile.spaceStuff.map(s => s.inspect).join(" ")
+    if (i) {
+        str.push(i)
     }
     if (signals.length > 0) {
         str.push("Looks like the radio is picking up a signal... It might just be noise, though.")
@@ -25,26 +24,11 @@ export const inspect = (Player, Map) => {
         str.push("The ship is running low on power. Let's try not to get stranded.")
     }
 
-    let neighbor_str = [];
-    neighbors.forEach(n => {
-        if (n.event) {
-            neighbor_str.push("There's something going on over in " + n.name + '.')
-        }
-        if (n.star) {
-            neighbor_str.push("It looks like there's a star system in " + n.name + '.')
-        }
-    })
     if (str.length == 0) {
         str = [
             'Space is empty and vast.',
         ]
     }
-    if (neighbor_str.length == 0) {
-        neighbor_str = [
-            "It doesn't look like there's much nearby. "
-        ]
-    }
-    let inspectStr = pickRandom(str) + ' ' + pickRandom(neighbor_str)
-    return inspectStr
+    return str.join(" ")
 
 }
